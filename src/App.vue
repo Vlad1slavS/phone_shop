bae63fdda947e0641d24645ddbb1de21e4b81637
Vlad <xstep759@gmail.com>
2024-09-30 21:21:59 +0900

    header in work

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@ import HelloWorld from './components/header.vue'
 </script>
 
 <template>
-  <div class="container">
-    <header class="bg-[#A5D364]">
-      <img src="" alt="">
-      <h1 class="text-3xl font-bold text-center">СОТКА</h1>
+  <div class="container w-1/4">
+    <header class="bg-[#A5D364] h-16 flex items-center">
+      <img class="h-10 w-10 mr-2" src="./assets/battery.svg" alt="">
+      <h1 class="text-3xl font-bold text-white">СОТКА</h1>
     </header>
     <HelloWorld />
   </div>
