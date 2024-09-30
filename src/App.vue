b9c65e272ed6f3005e0b1178173404d4ddc4a827
Vlad <xstep759@gmail.com>
2024-09-30 22:56:25 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,10 @@ export default {
       <h1 class="text-3xl font-bold text-white">СОТКА</h1>
     </header>
     <div class="grid grid-cols-2 gap-2 justify-center mt-16">
-      <CategoryCart categoryName="Iphone" imageSrc='src/assets/category_iphones.jpg' />
-      <CategoryCart categoryName="Macbooks" imageSrc='src/assets/mac_category.jpg' />
-      <CategoryCart categoryName="AirPods" imageSrc='src/assets/airpods_category.jpg' />
-      <CategoryCart categoryName="Аксесуары" imageSrc='src/assets/accessories_category.jpg' />
+      <CategoryCart categoryName="Iphone" imageSrc='/src/assets/category_iphones.jpg' />
+      <CategoryCart categoryName="Macbooks" imageSrc='/src/assets/mac_category.jpg' />
+      <CategoryCart categoryName="AirPods" imageSrc='/src/assets/airpods_category.jpg' />
+      <CategoryCart categoryName="Аксесуары" imageSrc='/src/assets/accessories_category.jpg' />
     </div>
     <button class="bg-[#64A4D3] w-full h-12 font-bold items-center px-4 text-white rounded-2xl">КОРЗИНА</button>
   </div>
