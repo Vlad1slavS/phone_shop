50f0cf3ccf3c004102162f1fe336b1b783189811
Vlad <xstep759@gmail.com>
2024-09-30 21:25:25 +0900

    bgc-white

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/header.vue'
 </script>
 
 <template>
-  <div class="container w-1/4">
+  <div class="container w-full">
     <header class="bg-[#A5D364] h-16 flex items-center">
       <img class="h-10 w-10 mr-2" src="./assets/battery.svg" alt="">
       <h1 class="text-3xl font-bold text-white">СОТКА</h1>
