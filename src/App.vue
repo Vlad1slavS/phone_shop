e144487f4169fc1faa8fe4a3446632ffe90e3245
Vlad <xstep759@gmail.com>
2024-09-30 18:35:41 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/header.vue'
+</script>
+
+<template>
+  <div class="container">
+    <header class="bg-[#A5D364]">
+      <img src="" alt="">
+      <h1>СОТКА</h1>
+    </header>
+    <HelloWorld />
+  </div>
+
+</template>
+
+<style scoped></style>
