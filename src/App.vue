c27b3d79b9916ef0e069c3aa8b0fb38341f6144f
Vlad <xstep759@gmail.com>
2024-09-30 22:03:42 +0900

    category_added

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,30 @@ import HelloWorld from './components/header.vue'
 
 <template>
   <div class="container w-full">
-    <header class="bg-[#A5D364] h-16 flex items-center">
+    <header class="bg-[#64A4D3] h-16 flex items-center px-4 justify-center">
       <img class="h-10 w-10 mr-2" src="./assets/battery.svg" alt="">
       <h1 class="text-3xl font-bold text-white">СОТКА</h1>
     </header>
-    <HelloWorld />
+    <div class="grid grid-cols-2 gap-2 justify-center mt-16">
+      <div class="category_item flex flex-col items-center">
+        <img class="w-64" src="./assets/category_iphones.jpg" alt="">
+        <a class=" uppercase" href="">Iphone</a>
+      </div>
+      <div class="category_item flex flex-col items-center">
+        <img class="w-64" src="./assets/mac_category.jpg" alt="">
+        <a href="">Macbooks</a>
+      </div>
+      <div class="category_item flex flex-col items-center">
+        <img class="w-64" src="./assets/category_iphones.jpg" alt="">
+        <a href="">Iphone</a>
+      </div>
+      <div class="category_item flex flex-col items-center">
+        <img class="w-64" src="./assets/category_iphones.jpg" alt="">
+        <a href="">Iphone</a>
+      </div>
+    </div>
+
+    <button class="bg-[#64A4D3] w-full h-12 font-bold items-center px-4 text-white rounded-2xl">КОРЗИНА</button>
   </div>
 
 </template>
