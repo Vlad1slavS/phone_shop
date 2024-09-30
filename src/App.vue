76952e949e29b38c4fd5f401b80fad7e040cb7f8
Vlad <xstep759@gmail.com>
2024-09-30 23:10:51 +0900

    color change

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,17 @@ export default {
 
 <template>
   <div class="container w-full">
-    <header class="bg-[#64A4D3] h-16 flex items-center px-4 justify-center">
+    <header class="bg-[#539DD3] h-16 flex items-center px-4 justify-center">
       <img class="h-10 w-10 mr-2" src="./assets/battery.svg" alt="">
       <h1 class="text-3xl font-bold text-white">СОТКА</h1>
     </header>
     <div class="grid grid-cols-2 gap-2 justify-center mt-16">
-      <CategoryCart categoryName="Iphone" imageSrc='/assets/category_iphones.jpg' />
-      <CategoryCart categoryName="Macbooks" imageSrc='/assets/mac_category.jpg' />
-      <CategoryCart categoryName="AirPods" imageSrc='/assets/airpods_category.jpg' />
-      <CategoryCart categoryName="Аксесуары" imageSrc='/assets/accessories_category.jpg' />
+      <CategoryCart categoryName="Iphone" imageSrc='src/assets/category_iphones.jpg' />
+      <CategoryCart categoryName="Macbooks" imageSrc='src/assets/mac_category.jpg' />
+      <CategoryCart categoryName="AirPods" imageSrc='src/assets/airpods_category.jpg' />
+      <CategoryCart categoryName="Аксесуары" imageSrc='src/assets/accessories_category.jpg' />
     </div>
-    <button class="fixed bottom-0 left-0 bg-[#64A4D3] w-full h-12 font-bold text-white rounded-2xl">
+    <button class="fixed bottom-0 left-0 bg-[#539DD3] w-full h-12 font-bold text-white rounded-2xl">
       КОРЗИНА
     </button>
   </div>
