b92c004b45bc5dd6c9388268c7fa135f3fdb4e9d
Vlad <xstep759@gmail.com>
2024-10-01 22:44:24 +0900

    fix scroll

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ export default {
 </script>
 
 <template>
-  <div class="container w-full pt-4 px-2">
-    <header class=" h-16 flex items-center px-4 justify-between mb-10">
+  <div class="container w-full pt-4 px-2 max-w-[414px] overflow-hidden">
+    <header class=" h-16 flex items-center  justify-between mb-10">
       <div class="flex"> <img class="h-10 w-10 mr-2" src="./assets/battery.svg" alt="">
         <h1 class="text-3xl font-bold text-black">СОТКА</h1>
       </div>
@@ -21,23 +21,29 @@ export default {
       </div>
     </header>
     <h1 class="text-3xl font-bold text-center mb-6">Что нового?</h1>
-    <div class="bg-black max-w-md mx-auto flex justify-center items-center py-10 rounded-2xl mb-11">
-      <div class="mr-10 text-center text-white">
-        <h2 class="text-3xl font-bold">Iphone 16</h2>
-        <p class="text-md">Еще больше возможностей</p>
-      </div>
-      <div>
-        <img class="h-28" src="./assets/iph16.png" alt="">
+    <div class="justify-center">
+      <div class="bg-black max-w-md mx-auto flex justify-center items-center py-10 rounded-2xl mb-11">
+        <div class="mr-10 text-center text-white">
+          <h2 class="text-3xl font-bold">Iphone 16</h2>
+          <p class="text-md">Еще больше возможностей</p>
+        </div>
+        <div>
+          <img class="h-28" src="./assets/iph16.png" alt="">
+        </div>
       </div>
     </div>
 
+
     <!-- <Slider /> -->
+    <div class="overflow-x-auto whitespace-nowrap">
       <div class="flex">
         <p class="text-lg text-white bg-slate-700 px-4 py-2 rounded-3xl mr-3">Iphone</p>
         <p class="text-lg text-white bg-slate-700 px-4 py-2 rounded-3xl mr-3">Macbook</p>
         <p class="text-lg text-white bg-slate-700 px-4 py-2 rounded-3xl mr-3">AirPods</p>
-        <p class="text-lg text-white bg-slate-700 px-4 py-2 rounded-3xl mr-3">AirPods</p>
+        <p class="text-lg text-white bg-slate-700 px-4 py-2 rounded-3xl mr-3">Аксесуары</p>
       </div>
+    </div>
+
 
 
     <div class=" justify-center mt-16">
